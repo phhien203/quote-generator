f035c1262a55f4e9ef8098995cf50295ac69d342
Hien Pham <phhien203@gmail.com>
2021-01-07 16:42:07 +0700

    use local quotes

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+// function showLoadingSpinner() {
+//     loader.hidden = false;
+//     quoteContainer.hidden = true;
+// }
 
-    try {
-        const res = await fetch(proxyUrl + apiUrl);
-        const quote = await res.json();
-        quoteText.innerText = quote.quoteText;
-        authorText.innerText = quote.quoteAuthor || 'Unknown';
+// function hideLoadingSpinner() {
+//     if (!loader.hidden) {
+//         loader.hidden = true;
+//         quoteContainer.hidden = false;
+//     }
+// }
 
-        if (quote.quoteText.length > 120) {
-            quoteText.classList.addClass('long-quote');
-        } else {
-            quoteText.classList.removeClass('long-quote');
-        }
-    } catch (error) {
-        console.log('Oops, no quote', error);
-        getQuote();
-    }
-}
+// async function getQuote() {
+//     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+//     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+//     try {
+//         showLoadingSpinner();
+//         const res = await fetch(proxyUrl + apiUrl);
+//         const quote = await res.json();
+//         quoteText.innerText = quote.quoteText;
+//         authorText.innerText = quote.quoteAuthor || 'Unknown';
+
+//         if (quote.quoteText.length > 120) {
+//             quoteText.classList.addClass('long-quote');
+//         } else {
+//             quoteText.classList.removeClass('long-quote');
+//         }
+//         hideLoadingSpinner();
+//     } catch (error) {
+//         console.log('Oops, no quote', error);
+//         getQuote();
+//     }
+// }
 
 function tweetQuote() {
     const text = quoteText.innerText;
@@ -31,7 +47,18 @@ function tweetQuote() {
     window.open(url, '_blank');
 }
 
-newQuoteBtn.addEventListener('click', getQuote);
+newQuoteBtn.addEventListener('click', () => {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    quoteText.innerText = quote.text;
+    authorText.innerText = quote.author || 'Unknown';
+
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+});
+
 twitterBtn.addEventListener('click', tweetQuote);
 
 // getQuote();
